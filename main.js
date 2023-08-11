01a00db61bfe6ededd12160ea9c98078c331937d
Joseph Innocent <mrrfabulous8@gmail.com>
2023-08-11 09:48:22 -0700

    addes slider
    added slider

--- main.js
+++ main.js
@@ -1,3 +1,59 @@
+
+
+let size = 1; // Initial size value
+
+function updateSize() {
+  const windowWidth = window.innerWidth;
+
+  if (windowWidth < 320) {
+    size = 1;
+  } else if (windowWidth < 500) {
+    size = 2;
+  } else if (windowWidth < 780) {
+    size = 2;
+  } else if (windowWidth < 900) {
+    size = 3;
+  } else if (windowWidth > 1000) {
+    size = 4;
+  }
+
+  // Perform any other actions based on the updated size value here
+  console.log(`Size updated to: ${size}`);
+}
+
+// Attach the 'resize' event listener to the window
+window.addEventListener('resize', updateSize);
+
+// Initial call to updateSize to set the initial size based on the initial window width
+updateSize();
+
+
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: size,
+  centeredSlides: false,
+  spaceBetween: 30,
+  loop: true,
+  fade: 'true',
+  grabCursor: 'true',
+  pagination: {
+    el: ".swiper-pagination",
+        clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
+
+
+
+
+
+
+
 var acc = document.getElementsByClassName("accordion");
 var i;
 
